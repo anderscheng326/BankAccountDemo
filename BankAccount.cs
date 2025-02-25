85f454e126efd5bfb8f0a1ff683243715603c683
anderscheng326 <anderscheng326@gmail.com>
2025-02-25 09:45:02 -0500

    create UML

---
+++ BankAccount.cs
@@ -0,0 +1,20 @@
+/*
+UML
+Class: BankAccount
+ Fields:
+    -accountID: str
+    -balance: float
+    -transactions: List<Transaction>
+
+ Properties:
+    +AccountID: str
+    +Balance: float
+    +Transaction: List<Transaction>
+
+ Methods:
+    +Withdraw(amount: float): void
+    +Deposit(amount: float): void
+
+ Constructor:
+    +BankAccount(name: str, initalBalance: float)
+*/
