e9546ae8fe45c9cd391bb654ff0dcb7667ecd72f
anderscheng326 <anderscheng326@gmail.com>
2025-02-25 10:36:03 -0500

    implement BankAccount class and test cases

--- BankAccount.cs
+++ BankAccount.cs
@@ -4,12 +4,12 @@ Class: BankAccount
  Fields:
     -accountID: str
     -balance: float
-    -transactions: List<Transaction>
+    -transactions: List<string>
 
  Properties:
     +AccountID: str
     +Balance: float
-    +Transaction: List<Transaction>
+    +Transactions: List<string>
 
  Methods:
     +Withdraw(amount: float): void
@@ -18,3 +18,61 @@ Class: BankAccount
  Constructor:
     +BankAccount(name: str, initalBalance: float)
 */
+
+using System.Diagnostics.Contracts;
+using System.Transactions;
+
+public class BankAccount
+{
+    string accountID;
+    float balance;
+    List<string> transactions = new List<string>();
+
+    public BankAccount(string name, float initialBalance)
+    {
+        this.accountID = name;
+        this.balance = initialBalance;
+        this.transactions = new List<string>();
+        transactions.Add($"New Account:{accountID} - Initial Balance: ${balance}");
+    }
+
+    public string AccountID
+    {
+        get {return accountID;}
+    }
+
+    public float Balance
+    {
+        get {return balance;}
+    } 
+
+    public List<string> Transactions
+    {
+        get {return transactions;}
+    }
+
+    public void Deposit(float amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            transactions.Add($"-Deposit: $ {amount} - Updated Balance: ${balance}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid deposit.");
+        }
+    }
+    public void Withdraw(float amount)
+    {
+        if (amount > 0 && balance > amount)
+        {
+            balance -= amount;
+            transactions.Add($"-Withdraw: $ {amount} - Updated Balance: ${balance}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid withdrawal.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+
+public class Test
+{
+    public static void Main(string[] args)
+     { 
+        BankAccount acc1 = new BankAccount("bob", 1000);
+        acc1.Deposit(1524.4f);
+        acc1.Deposit(-200);
+        acc1.Withdraw(1000.20f);
+        acc1.Withdraw(500000);
+
+        //printing list of transactions
+        foreach (string transactions in acc1.Transactions)
+        {
+            Console.WriteLine(transactions);
+        }
+    }
+}
